364e21a7894620de37aacb73734ffaeb0a8c7b97
wailovet <wailovet@163.com>
2015-12-29 13:30:09 +0800

    update

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ###使用 
 ```javascript
 MiniPage.ready(function(){
-    //当前页
+    //当前页数
     var cur_page = 10;
     //总数据条数
     var all_item = 100;
@@ -12,13 +12,17 @@ MiniPage.ready(function(){
     //展示可见列表
     var showing_page_count = 5;
     mp = new MiniPage(10,180,12,8);
-    console.log("当前页");
+    console.log("当前条数");
     console.log(cur_page);
     console.log("上一页");
     console.log(mp.getPrepPage());
-    console.log("可见页列表");
+    console.log("可见页");
     console.log(mp.getShowingPageToArray());
     console.log("下一页");
     console.log(mp.getNextPage());
+    console.log("总页数");
+    console.log(mp.getPageCount());
+    console.log("当前项数");
+    console.log(mp.getItemCount());
 });
 ```
--- src/MiniPage.js
+++ src/MiniPage.js
@@ -1,57 +1,62 @@
 /**
  * Created by Administrator on 2015/11/30.
  */
-define(['jquery'],function(){
-    return function(cur_page,all_item,limit,showing_page_count){
+define(['jquery'], function () {
+    return function (cur_page, all_item, limit, showing_page_count) {
         var look_page_count = showing_page_count;
         var look_page_arr = new Array;
-        if(!cur_page)cur_page = 1;
-        var all_page = parseInt(all_item/limit);
-        if(all_item%limit != 0){
+        if (!cur_page)cur_page = 1;
+        var all_page = parseInt(all_item / limit);
+        if (all_item % limit != 0) {
             all_page++;
         }
 
-        if(cur_page>all_page){
+        if (cur_page > all_page) {
             throw "page error";
         }
-        if(cur_page < look_page_count/2){
-            for(var i =0;i<look_page_count &&i+1 > 0&&i+1 <= all_page ;i++){
-                look_page_arr[i] = i+1;
+
+        look_page_arr.push(cur_page);
+        for (var i = 1; look_page_arr.length < look_page_count; i++) {
+            if (cur_page + i <= all_page) {
+                look_page_arr.push(cur_page + i);
             }
-        }else {
-            if(cur_page > all_page - look_page_count/2){
-                for(var i =0;i<look_page_count;i++){
-                    if(all_page - look_page_count + i +1 > 0&&  all_page - look_page_count + i +1 <= all_page ){
-                        look_page_arr[i] = all_page - look_page_count + i +1;
-                    }
-                }
-            }else{
-                for(var i =0;i<look_page_count ;i++){
-                    look_page_arr[i] = cur_page - 1 + i ;
-                }
+            if (cur_page - i > 0) {
+                look_page_arr.unshift(cur_page - i);
+            }
+            if (cur_page - i <= 0 && cur_page + i >= all_page) {
+                break;
             }
         }
 
+
         var pre_page = null;
         var next_page = null;
-        if(cur_page > 1){
+        var item_count = limit;
+        if (cur_page == all_page) {
+            item_count = all_item - ((cur_page - 1) * limit);
+        }
+
+        if (cur_page > 1) {
             pre_page = cur_page - 1;
         }
-        if(cur_page < all_page){
+        if (cur_page < all_page) {
             next_page = cur_page + 1;
         }
         return {
-            getPrepPage:function(){
+            getPrepPage: function () {
                 return pre_page;
             },
-            getNextPage:function(){
+            getNextPage: function () {
                 return next_page;
             },
-            getShowingPageToArray:function(){
+            getShowingPageToArray: function () {
                 return look_page_arr;
             },
-            getPageCount:function(){
+            getPageCount: function () {
                 return all_page;
+            },
+            getItemCount: function () {
+                return item_count;
             }
         };
     }
